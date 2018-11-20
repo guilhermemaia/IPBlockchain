4e239d32a3fcf3de2959f6e93ad7939f2272732b
guilhermemaia <guilherme@pilarcultural.org>
2018-11-20 00:27:11 -0200

    Update iniciativaPopular1.sol

--- contrato/iniciativaPopular1.sol
+++ contrato/iniciativaPopular1.sol
@@ -15,13 +15,19 @@ contract LeiDeIniciativaPopular {
 	string unidadeDaFerderacao;
 	string municipio;
     }
+	
 	uint contador_signatarios;
-	mapping (address => signatario) signatarios;
+	mapping (uint => signatario) signatarios;
 	
 	function novaAssinatura (address _contaETH, string _nomeSignatario, string _tituloDeLeleitor, string _enderecoSignatario, string _unidadeDaFerderacao, string _municipio) public {
     	signatarios[contador_signatarios] = signatario(_contaETH, _nomeSignatario, _tituloDeLeleitor, _enderecoSignatario, _unidadeDaFerderacao, _municipio);
     	contador_signatarios++;
-}
+    }
+    
+    	function getAssinaturas (address contaETH) public returns (string) {
+    	for (uint i = 0 ; i < contacts_count; i++) {
+            return contador_signatarios.nomeSignatario;
+    }
 		
     modifier somenteProponente() {
         require(msg.sender==proponente, "Somente o proponente desta iniciativa pode realizar esta operação");
