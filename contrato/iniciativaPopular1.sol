21d7aee5cf2efad351b66768bdca8fc3a90cfd0f
guilhermemaia <guilherme@pilarcultural.org>
2018-11-20 00:12:50 -0200

    Update iniciativaPopular1.sol

--- contrato/iniciativaPopular1.sol
+++ contrato/iniciativaPopular1.sol
@@ -8,30 +8,29 @@ contract LeiDeIniciativaPopular {
     address proponente;
     
     struct signatario {
-        id = 0;
         address contaETH;
         string nomeSignatario;
 	string tituloDeLeleitor;
 	string enderecoSignatario;
 	string unidadeDaFerderacao;
 	string municipio;
-	unit assinado;
     }
-	
+	uint contador_signatarios;
 	mapping (address => signatario) signatarios;
-	uint assinado;
-	uint id;
-	 
-	constructor() public {
-        tituloDaIniciativa = "Proposta de Lei de Iniciativa Popular";
-        proponente = msg.sender;
-	assinado  = 0;
-  	id = 0;
-    }
+	
+	function novaAssinatura (address _contaETH, string _nomeSignatario, string _tituloDeLeleitor, string _enderecoSignatario, string _unidadeDaFerderacao, string _municipio) public {
+    	signatarios[contador_signatarios] = signatario(_contaETH, _nomeSignatario, _tituloDeLeleitor, _enderecoSignatario, _unidadeDaFerderacao, _municipio);
+    	contador_signatarios++;
+}
 		
     modifier somenteProponente() {
         require(msg.sender==proponente, "Somente o proponente desta iniciativa pode realizar esta operação");
         _;
+    }
+
+    constructor() public {
+        tituloDaIniciativa = "Proposta de Lei de Iniciativa Popular";
+        proponente = msg.sender;
     }
 
     function definirTituloDaIniciativa(string qualTituloDaIniciativa) public somenteProponente  {
@@ -45,15 +44,5 @@ contract LeiDeIniciativaPopular {
     function definirTextoDaProposta(string qualTextoDaProposta) public somenteProponente  {
         textoDaProposta = qualTextoDaProposta;
     }
-    
-    function incluirAssinatura(string _nomeSignatario) public {
-    id=id+1;
-    signatarios[id] = signatario(id, contaETH, nomeSignatario, tituloDeLeleitor, enderecoSignatario, unidadeDaFerderacao, municipio, assinado);
-  }
-
-  function getAssinaturas(uint _id) public view returns(string) {
-   return signatarios[_id].nomeSignatario.tituloDeLeleitor;
-  }
-    
     
   }   
