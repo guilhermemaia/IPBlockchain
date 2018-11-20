29cc21a33970e55855a9ea1ab5f1f93aa29c44e8
guilhermemaia <guilherme@pilarcultural.org>
2018-11-19 23:01:35 -0200

    Update iniciativaPopular1.sol

--- contrato/iniciativaPopular1.sol
+++ contrato/iniciativaPopular1.sol
@@ -2,35 +2,36 @@ pragma solidity 0.4.25;
 
 contract LeiDeIniciativaPopular {
 	
-    address proponente;
-    
-    struct proposta {
     string public tituloDaIniciativa;
     string public ementaDaProposta;
     string public textoDaProposta;
-    bool assinado;
-    }
-       
+    address proponente;
+    
     struct signatario {
+        id = 0;
         address contaETH;
         string nomeSignatario;
 	string tituloDeLeleitor;
 	string enderecoSignatario;
 	string unidadeDaFerderacao;
 	string municipio;
+	unit assinado;
     }
 	
 	mapping (address => signatario) signatarios;
-	
+	uint assinado;
+	uint id;
+	 
+	constructor() public {
+        tituloDaIniciativa = "Proposta de Lei de Iniciativa Popular";
+        proponente = msg.sender;
+	assinado  = 0;
+  	id = 0;
+    }
 		
     modifier somenteProponente() {
         require(msg.sender==proponente, "Somente o proponente desta iniciativa pode realizar esta operação");
         _;
-    }
-
-    constructor() public {
-        tituloDaIniciativa = "Proposta de Lei de Iniciativa Popular";
-        proponente = msg.sender;
     }
 
     function definirTituloDaIniciativa(string qualTituloDaIniciativa) public somenteProponente  {
@@ -44,5 +45,15 @@ contract LeiDeIniciativaPopular {
     function definirTextoDaProposta(string qualTextoDaProposta) public somenteProponente  {
         textoDaProposta = qualTextoDaProposta;
     }
+    
+    function incluirAssinatura(string _nomeSignatario) public {
+    id=id+1;
+    signatarios[id] = signatario(id, contaETH, nomeSignatario, tituloDeLeleitor, enderecoSignatario, unidadeDaFerderacao, municipio, assinado);
+  }
+
+  function getAssinaturas(uint _id) public view returns(string) {
+   return signatarios[_id].nomeSignatario.tituloDeLeleitor;
+  }
+    
     
   }   
