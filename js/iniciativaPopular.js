242241479ffa1cf6feaa199c6036052eb79888ce
guilhermemaia <guilherme@pilarcultural.org>
2018-11-21 23:34:32 -0200

    Update iniciativaPopular.js

--- js/iniciativaPopular.js
+++ js/iniciativaPopular.js
@@ -107,7 +107,9 @@ function definirAgente() {
         }
     });
   
-  [
+	// Nova parte inicia aqui
+	
+  const contratoLeiDeIniciativaPopularABI = [
 	{
 		"constant": false,
 		"inputs": [
@@ -248,3 +250,52 @@ function definirAgente() {
 	}
 ]
 }
+];
+
+var contratoLeiDeIniciativaPopular = web3.eth.contract(contratoLeiDeIniciativaPopularABI).at("0x3ff10b9a9e4a52ec9a652226b7d9f8f8a1e12282");
+
+function obtemNomeEmpresa() {
+    contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.log("Erro");
+            console.error(err);
+        } else {
+            console.log("Resultado");
+            let objStatus = document.getElementById("spanNomeEmpresa");
+            console.log(resultado);
+            objStatus.innerText = resultado;
+        }
+    });
+}
+
+function registrarNomeEmpresa() {
+	var statusTransacao = document.getElementById("statusTransacaoNomeEmpresa");
+	var nomeEmpresa = document.formNomeEmpresa.campoNomeEmpresa.value;
+	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
+	contratoUsoDeImagem.definirNomeDaEmpresa(nomeEmpresa, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.log("Erro");
+			console.error(err);
+			statusTransacao.innerHTML = "Erro: " + err;
+        } else {
+            console.log("Resultado");
+            console.log(resultado);
+            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
+        }
+    });
+}
+function definirAgente() {
+	var statusTransacao = document.getElementById("statusTransacaoNomeEmpresa");
+	var qualAgente = document.formDefinirAgente.campoDefinirAgente.value;
+	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
+	contratoUsoDeImagem.definirAgente(qualAgente, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.log("Erro");
+			console.error(err);
+			statusTransacao.innerHTML = "Erro: " + err;
+        } else {
+            console.log("Resultado");
+            console.log(resultado);
+            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
+        }
+    });
