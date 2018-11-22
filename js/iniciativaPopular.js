ca4d5067b2cb75ae68be6f639e0377ea4797bdee
guilhermemaia <guilherme@pilarcultural.org>
2018-11-21 23:52:35 -0200

    Update iniciativaPopular.js

--- js/iniciativaPopular.js
+++ js/iniciativaPopular.js
@@ -1,114 +1,3 @@
-
-const contratoUsoDeImagemABI = [
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "nomeEmpresa",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "receberPeloUso",
-		"outputs": [],
-		"payable": true,
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "qualAgente",
-				"type": "address"
-			}
-		],
-		"name": "definirAgente",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "qualNomeDaEmpresa",
-				"type": "string"
-			}
-		],
-		"name": "definirNomeDaEmpresa",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	}
-];
-
-var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0x3ff10b9a9e4a52ec9a652226b7d9f8f8a1e12282");
-
-function obtemNomeEmpresa() {
-    contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
-        if (err)    {
-            console.log("Erro");
-            console.error(err);
-        } else {
-            console.log("Resultado");
-            let objStatus = document.getElementById("spanNomeEmpresa");
-            console.log(resultado);
-            objStatus.innerText = resultado;
-        }
-    });
-}
-
-function registrarNomeEmpresa() {
-	var statusTransacao = document.getElementById("statusTransacaoNomeEmpresa");
-	var nomeEmpresa = document.formNomeEmpresa.campoNomeEmpresa.value;
-	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
-	contratoUsoDeImagem.definirNomeDaEmpresa(nomeEmpresa, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
-        if (err)    {
-            console.log("Erro");
-			console.error(err);
-			statusTransacao.innerHTML = "Erro: " + err;
-        } else {
-            console.log("Resultado");
-            console.log(resultado);
-            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
-        }
-    });
-}
-function definirAgente() {
-	var statusTransacao = document.getElementById("statusTransacaoNomeEmpresa");
-	var qualAgente = document.formDefinirAgente.campoDefinirAgente.value;
-	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
-	contratoUsoDeImagem.definirAgente(qualAgente, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
-        if (err)    {
-            console.log("Erro");
-			console.error(err);
-			statusTransacao.innerHTML = "Erro: " + err;
-        } else {
-            console.log("Resultado");
-            console.log(resultado);
-            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
-        }
-    });
-  
-	// Nova parte inicia aqui
-	
   const contratoLeiDeIniciativaPopularABI = [
 	{
 		"constant": false,
@@ -254,48 +143,16 @@ function definirAgente() {
 
 var contratoLeiDeIniciativaPopular = web3.eth.contract(contratoLeiDeIniciativaPopularABI).at("0x3ff10b9a9e4a52ec9a652226b7d9f8f8a1e12282");
 
-function obtemNomeEmpresa() {
-    contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+function obterTituloDaIniciativa() {
+    contratoLeiDeIniciativaPopular.qualTituloDaIniciativa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
         if (err)    {
             console.log("Erro");
             console.error(err);
         } else {
             console.log("Resultado");
-            let objStatus = document.getElementById("spanNomeEmpresa");
+            let objStatus = document.getElementById("spanTituloDaIniciativa");
             console.log(resultado);
             objStatus.innerText = resultado;
         }
     });
 }
-
-function registrarNomeEmpresa() {
-	var statusTransacao = document.getElementById("statusTransacaoNomeEmpresa");
-	var nomeEmpresa = document.formNomeEmpresa.campoNomeEmpresa.value;
-	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
-	contratoUsoDeImagem.definirNomeDaEmpresa(nomeEmpresa, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
-        if (err)    {
-            console.log("Erro");
-			console.error(err);
-			statusTransacao.innerHTML = "Erro: " + err;
-        } else {
-            console.log("Resultado");
-            console.log(resultado);
-            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
-        }
-    });
-}
-function definirAgente() {
-	var statusTransacao = document.getElementById("statusTransacaoNomeEmpresa");
-	var qualAgente = document.formDefinirAgente.campoDefinirAgente.value;
-	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
-	contratoUsoDeImagem.definirAgente(qualAgente, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
-        if (err)    {
-            console.log("Erro");
-			console.error(err);
-			statusTransacao.innerHTML = "Erro: " + err;
-        } else {
-            console.log("Resultado");
-            console.log(resultado);
-            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
-        }
-    });
