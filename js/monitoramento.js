3823c19ed2842976579a43709d0b2d6c14206b9b
guilhermemaia <guilherme@pilarcultural.org>
2018-11-29 01:24:45 -0200

    novas funções para monitoramento de detalhes da proposta

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -11,3 +11,49 @@ function obterTotalDeAssinaturas() {
         }
     });
 }
+
+
+function obterTituloDaProposta() {
+    contratoLeiDeIniciativaPopular.tituloDaIniciativa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) 
+    {
+        if (err)    {
+            console.log("Erro");
+            console.error(err);
+        } else {
+            console.log("Resultado");
+            let objStatus = document.getElementById("tituloDaProposta");
+            // console.log(resultado);
+			objStatus.innerText = resultado;
+        }
+    });
+}
+
+function obterEmentaDaProposta() {
+    contratoLeiDeIniciativaPopular.ementaDaProposta({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) 
+    {
+        if (err)    {
+            console.log("Erro");
+            console.error(err);
+        } else {
+            console.log("Resultado");
+            let objStatus = document.getElementById("ementaDaProposta");
+            // console.log(resultado);
+			objStatus.innerText = resultado;
+        }
+    });
+}
+
+function obterTextDaProposta() {
+    contratoLeiDeIniciativaPopular.textoDaProposta({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) 
+    {
+        if (err)    {
+            console.log("Erro");
+            console.error(err);
+        } else {
+            console.log("Resultado");
+            let objStatus = document.getElementById("textDaProposta");
+            // console.log(resultado);
+			objStatus.innerText = resultado;
+        }
+    });
+}
