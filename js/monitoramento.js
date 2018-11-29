d708f3bedc4dfc382c53bef85bfbfbc36bd15405
guilhermemaia <guilherme@pilarcultural.org>
2018-11-29 00:59:50 -0200

    ajustes no monitoramento

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -1,5 +1,18 @@
 function obterTotalDeAssinaturas() {
-    contratoLeiDeIniciativaPopular.totalDeAssinaturas({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
-		let resultado = document.getElementById("totalAssinaturas");
-		console.log(obterTotalDeAssinaturas);
-	}
+//     contratoLeiDeIniciativaPopular.totalDeAssinaturas({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+//         if (err)    {
+//             console.log("Erro");
+//             console.error(err);
+//         } else {
+//             console.log("Resultado");
+//             let objStatus = document.getElementById("totalAssinaturas");
+//             // console.log(resultado);
+// 			objStatus.innerText = resultado;
+//         }
+//     });
+// }
+
+contratoLeiDeIniciativaPopular.totalDeAssinaturas({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        let resultado = document.getElementById("totalAssinaturas");
+});
+}
