3d4fbea2e4b93ce90d93b196c2d834e99768ce48
guilhermemaia <guilherme@pilarcultural.org>
2018-11-29 00:30:00 -0200

    ajustes

--- js/enderecoEthereum.js
+++ js/enderecoEthereum.js
@@ -3,8 +3,8 @@ function getEnderecoEthereum() {
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
   localStorage.setItem('enderecoEthereum', enderecoEthereum); 
- // window.location = "https://guilhermemaia.github.io/IPBlockchain/assinaturas.html";
-  window.location = "http://127.0.0.1:5500/assinaturas.html";
+  window.location = "https://guilhermemaia.github.io/IPBlockchain/assinaturas.html";
+ // window.location = "http://127.0.0.1:5500/assinaturas.html";
 }
 
 
